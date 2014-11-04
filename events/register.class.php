b40956b3a11c030744824c58f0ffd49c64f63293
Ahmet <ahmet.atay@hotmail.com>
2014-11-04 15:18:16 +0200

    revision

--- events/register.class.php
+++ events/register.class.php
@@ -18,62 +18,118 @@ class Register {
 	}
 	
 	/**
-	 * CodeIgniter ortam değişkenini (get_instance) static değişkenine aktarır
+	 * transfer to static variable of the ci environment variable
 	 *
 	 * @return no return
 	 */
 	public static function Instance(&$instance)
 	{
-		// Codeignter instance
+		// Codeignter environment instance
 		self::$CI = $instance;
 	}
 	
 	/**
-	 * Gelen event anahtarı doğrultusunda event tetikler
+	 * Event triggers the switch in the direction of the incoming event
 	 *
 	 * @return no return
 	 */
-	public static function fire($key, $extra_parm = null)
+	public static function fire($key, $extra_parm = null, $callback_closure = false)
 	{
+		// checking parameter
+		if(is_null($callback_closure)){
+			if(self::isClosure($extra_parm)){
+				$callback_closure = $extra_parm;
+				$extra_parm = null;
+			}else{
+				$callback_closure = false;
+			}
+		}else{
+			// if parameter is incorrect
+			if(!self::_checkParameter($key, $extra_parm, $callback_closure)){
+				return false;
+			}
+		}
 		
-		// Tüm event'lerin çalıştırılması kontrol ediliyor..
+		// if run all event...
 		if($key == '*'){
-			return self::_runAll($extra_parm);
+			return self::_runAll($extra_parm, $callback_closure);
 		}
 		
+		// if work only if event has prefixes...
 		if(preg_match('/\*/', $key) > 0){
-			return self::_runPrefixes($key, $extra_parm);
+			return self::_runPrefixes($key, $extra_parm, $callback_closure);
 		}
 		
-		// event anahtarı kontrol ediliyor..
+		// check event key
 		if(isset(self::$closures[$key])){
-			// anahtar önce bir değişkene aktarılıyor..
+			// key transfer to variable
 			$_closure = self::$closures[$key];
 			
-			// sonrasında closure çalıştırılıyor
-			return $_closure(self::$CI, $extra_parm);
+			// running after event
+			$result = self::_runClosure($_closure, $extra_parm);
+			
+			// check and running callback
+			self::_runClosure($callback_closure, $result);
+			
+			return $result;
 		}
 	}
 	
-	private static function _runAll($extra_parm = array())
+	/**
+	 * Closure worker
+	 *
+	 * @return bool/array/string/null
+	 */
+	private static function _runClosure($closure, $data)
+	{
+		if($closure && self::isClosure($closure)){
+			return $closure(self::$CI, $data);
+		}
+	}
+	
+	/**
+	 * if will work if event has prefixes
+	 *
+	 * @return array
+	 */
+	private static function _runAll($extra_parm = array(), $callback_closure)
 	{
 		$result = array();
 		
+		// run all events
 		foreach(self::$closures as $key => $closure){
-			$parm = isset($extra_parm[$key]) ? $extra_parm[$key] : null;
-			$result[$key] = $closure(self::$CI, $parm);
+			if(isset($extra_parm['*'])){
+				$parm = $extra_parm['*'];
+			}else{
+				$parm = isset($extra_parm[$key]) ? $extra_parm[$key] : null;
+			}
+			
+			// closure result
+			$result[$key] = self::_runClosure($closure, $parm);
 		}
+		
+		// running callback
+		self::_runClosure($callback_closure, $result);
 		
 		return $result;
 	}
 	
-	private static function _runPrefixes($key = false, $extra_parm = array())
+	/**
+	 * all events trigger
+	 *
+	 * @return array / bool
+	 */
+	private static function _runPrefixes($key = false, $extra_parm = array(), $callback_closure)
 	{
 		if($key){
+			// event key parsing
 			$keyArr = preg_split('/\./',$key);
 			$newKey = '';
 			
+			// checking the parse result 
 			if(count($keyArr) > 1 && end($keyArr) == '*'){
+				
+				// the parse result  pass into loop without star
 				foreach($keyArr as $k){
 					if($k != '*'){
 						$newKey .= empty($newKey) ? $k : '.' . $k;
@@ -83,23 +139,30 @@ class Register {
 				if(!empty($newKey)){
 					$result = array();
 					
+					// the request prefixes is running
 					foreach(self::$closures as $key => $closure){
+						
+						// processing events if contain request prefix
 						if(preg_match('/'.preg_quote($newKey).'/', $key) > 0){
 							
+							// incoming parameters are to be run for all events are available
 							if(isset($extra_parm['*'])){
 								$parm = $extra_parm['*'];
 							}else{
+								// otherwise the event is sent to each parameter belongs
 								$parm = isset($extra_parm[$key]) ? $extra_parm[$key] : null;
 							}
 							
-							$result[$key] = $closure(self::$CI, $parm);
+							// results are added to the array
+							$result[$key] = self::_runClosure($closure, $parm);
 						}
 					}
 					
+					// running callback
+					self::_runClosure($callback_closure, $result);
+					
 					return $result;
 				}
-				
-				
 			}
 		}
 		
@@ -107,7 +170,34 @@ class Register {
 	}
 	
 	/**
-	 * Event kayıtlarının static closure dizisine katarır
+	 * check the parameters  when the event is triggered
+	 *
+	 * @return bool
+	 */
+	public static function _checkParameter($key, $extra_parm, $callback_closure)
+	{
+		$result = true;
+		
+		// event controls the type of key
+		if(!is_string($key)){
+			$result = false;
+		}
+		
+		// event cross-check the type of parameters
+		if(!is_null($extra_parm) && !is_array($extra_parm) && (!is_string($extra_parm) && !is_object($extra_parm) ||  self::isClosure($extra_parm))){
+			$result = false;
+		}
+		
+		// check of closure type
+		if(!self::isClosure($callback_closure)){
+			$result = false;
+		}
+		
+		return $result;
+	}
+	
+	/**
+	 * transfer to the closure  array of the event requests
 	 *
 	 * @return bool
 	 */
@@ -123,7 +213,7 @@ class Register {
 	}
 	
 	/**
-	 * Hali hazırda kayıt event'lerin varlığını kontrol eder
+	 * check asset of available events
 	 *
 	 * @return bool
 	 */
@@ -133,7 +223,7 @@ class Register {
 	}
 	
 	/**
-	 * Kayıtlı event'leri siler
+	 * delete the registered events
 	 *
 	 * @return bool
 	 */
@@ -144,5 +234,15 @@ class Register {
 			
 			return true;
 		}
+	}
+	
+	/**
+	 * check closure requests
+	 *
+	 * @return bool
+	 */
+	public function isClosure($closure)
+	{
+		return ($closure instanceof Closure);
 	}
 }
